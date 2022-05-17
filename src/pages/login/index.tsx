ce3067bd8b0da86fd8cdd6ccf2353d3117aca71e
Syafiqjos <syafiqjos@gmail.com>
2022-05-18 06:12:27 +0700

    feat: add base login page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+import Layout from '@/components/layout/Layout';
+import ArrowLink from '@/components/links/ArrowLink';
+import ButtonLink from '@/components/links/ButtonLink';
+import UnderlineLink from '@/components/links/UnderlineLink';
+import UnstyledLink from '@/components/links/UnstyledLink';
+import Seo from '@/components/Seo';
+
+/**
+ * SVGR Support
+ * Caveat: No React Props Type.
+ *
+ * You can override the next-env if the type is important to you
+ * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
+ */
+import Vercel from '~/svg/Vercel.svg';
+
+// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
+// Before you begin editing, follow all comments with `STARTERCONF`,
+// to customize the default configuration.
+
+export default function HomePage() {
+  return (
+    <Layout>
+      {/* <Seo templateTitle='Home' /> */}
+      <Seo />
+
+      <main>
+        <section className='bg-white'>
+          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
+            
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
