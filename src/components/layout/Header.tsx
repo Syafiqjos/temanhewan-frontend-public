d5a3d2bd1005a719a70ed993b2e728b9b206ab74
Syafiqjos <syafiqjos@gmail.com>
2022-05-18 06:09:14 +0700

    feat: implement new navbar header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,26 +1,33 @@
 import * as React from 'react';
 
 import UnstyledLink from '@/components/links/UnstyledLink';
+import ButtonLink from '@/components/links/ButtonLink';
 
 const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  { href: '/forum', label: 'Forum', type: '' },
+  { href: '/contact-us', label: 'Contact', type: '' },
+  { href: '/faq', label: 'FAQ', type: '' },
+  { href: '/about', label: 'About', type: '' },
+  { href: '/login', label: 'Login', type: 'primary' },
 ];
 
 export default function Header() {
   return (
     <header className='sticky top-0 z-50 bg-white'>
-      <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
+      <div className='layout flex h-16 py-2 items-center justify-between'>
+        <UnstyledLink href='/' className='font-bold hover:text-gray-600 flex'>
+          <img className='h-8' src="/images/logo.png" />
+          <img className='h-8' src="/images/logo-text.png" />
         </UnstyledLink>
         <nav>
-          <ul className='flex items-center justify-between space-x-4'>
-            {links.map(({ href, label }) => (
+          <ul className='flex items-center justify-between space-x-8 text-gray-500'>
+            {links.map(({ href, label, type }) => (
               <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
-                  {label}
-                </UnstyledLink>
+				{
+					type === 'primary'
+					? <ButtonLink href={href} variant='primary'>{label}</ButtonLink>
+					: <UnstyledLink href={href} className='hover:text-gray-600'>{label}</UnstyledLink>
+				}
               </li>
             ))}
           </ul>
