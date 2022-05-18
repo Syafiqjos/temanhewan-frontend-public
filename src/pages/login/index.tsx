b3db1c74b6ee95ef0bb0bd8663cc6f87e313cbf0
Syafiqjos <syafiqjos@gmail.com>
2022-05-18 14:04:45 +0700

    feat(login): implement form reusable components

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,6 +5,8 @@ import ArrowLink from '@/components/links/ArrowLink';
 import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
 import UnstyledLink from '@/components/links/UnstyledLink';
+import InputText from '@/components/forms/InputText';
+import InputButton from '@/components/forms/InputButton';
 import Seo from '@/components/Seo';
 
 /**
@@ -32,23 +34,15 @@ export default function HomePage() {
             <form className='flex flex-col items-start justify-start p-4 text-left gap-3'>
               <h1 className="text-xl font-semibold">Masuk Akun</h1>
               <h2 className="text-base font-normal">Dapatkan sensasi hewan peliharaan.</h2>
-              <div className="flex flex-col items-start w-full">
-                <label for="email">Email/Username</label>
-                <input className="bg-gray-100 border-0 rounded-l w-full p-4" type="text" name="email" id="email" placeholder="Email anda" />
-              </div>
-              <div className="flex flex-col items-start w-full">
-                <label for="password">Password</label>
-                <input className="bg-gray-100 border-0 rounded-l w-full p-4" type="password" name="password" id="password" placeholder="Password anda" />
-              </div>
+              <InputText label="Email" name="email" type="text" placeholder="Email anda" />
+              <InputText label="Password" name="password" type="password" placeholder="Password anda" />
               <div>
                 <span>Lupa password? <UnstyledLink href="/reset-password" className="text-orange-600">Klik disini</UnstyledLink></span>
               </div>
               <div>
                 <span>Belum pernah mendaftar? <UnstyledLink href="/sign-up" className="text-orange-600">Klik disini</UnstyledLink></span>
               </div>
-              <div>
-                <input className="bg-orange-600 text-white p-4 px-8 cursor-pointer rounded-xl" type="submit" value="Masuk" />
-              </div>
+              <InputButton text="Masuk" />
             </form>
             <div className="p-4">
               <img className="rounded-xl" src="/images/cover/login-cover.png" />
