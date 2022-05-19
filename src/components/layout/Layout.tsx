59182077d5d4aec55c3ff19d5e0beb74b616a496
Syafiqjos <syafiqjos@gmail.com>
2022-05-19 15:26:09 +0700

    fix(components): fix import typo

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Header from '@/components/layout/Header.tsx';
+import Header from '@/components/layout/Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
