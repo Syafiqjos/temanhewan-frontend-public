9cea5396716964f027680d78662d97afd94a8ba3
Syafiqjos <syafiqjos@gmail.com>
2022-05-26 06:43:06 +0700

    feat(auth): implement auth context in main layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import Header from '@/components/layout/Header';
+import { AuthProvider } from '@/providers/AuthContextProvider';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
   return <>
-    <Header/>
-    {children}
+	<AuthProvider>
+		<Header/>
+		{children}
+	</AuthProvider>
   </>;
 }
