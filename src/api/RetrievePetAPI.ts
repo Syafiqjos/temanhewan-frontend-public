c1b2d7052802484cc1676c10ca6b65b830e60056
Syafiqjos <syafiqjos@gmail.com>
2022-05-26 17:02:13 +0700

    feat(api): add retrieve pet api

---
+++ src/api/RetrievePetAPI.ts
@@ -0,0 +1,22 @@
+import * as API from '@/api/API';
+import AuthService from '@/services/AuthService';
+
+export default async function RetrievePetAPI(
+{
+	id
+}: {
+	id: string
+}) {
+	API.GetCSRFToken();
+	const authToken = AuthService.getToken();
+	console.log(authToken);
+	const url = API.GetAPIHost('/api/pet/get');
+	const body = { 
+		id
+	};
+	const res = await API.PostAPI(url, body, {
+		'Authorization': `Bearer ${authToken}`
+	});
+
+	return res
+}
