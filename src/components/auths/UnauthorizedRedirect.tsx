ea73e65a8eea1d598b2d1757c8026df5eb69f0c3
Syafiqjos <syafiqjos@gmail.com>
2022-05-26 11:22:00 +0700

    feat(auth): add prevent access to page that need to be login component

---
+++ src/components/auths/UnauthorizedRedirect.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+import { useRouter } from 'next/router';
+import { useAuthState } from '@/providers/AuthContextProvider';
+
+function BlockingAuth({ show, children }: { show: boolean, children: any }) {
+	return show && children;
+}
+
+export default function UnauthorizedRedirect({ children }: { children: React.Node }) {
+	const router = useRouter();
+	const authState = useAuthState();
+
+	const [ show, setShow ] = React.useState(false);
+
+	React.useEffect(() => {
+		if (!router.isReady) return;
+
+		if (authState.authenticated) {
+			setShow(false);
+			router.push('/');
+		} else if (authState.loading == false) {
+			setShow(true);
+		}
+
+	}, [ authState, authState.authenticated, router.isReady ]);
+
+	return <BlockingAuth show={show}>{children}</BlockingAuth>
+}
