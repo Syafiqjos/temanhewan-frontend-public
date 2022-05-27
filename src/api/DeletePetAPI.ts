5afdb81763be441dc69623516e98a7703aab2fdb
Syafiqjos <syafiqjos@gmail.com>
2022-05-27 13:53:00 +0700

    feat(api): add delete pet api

---
+++ src/api/DeletePetAPI.ts
@@ -0,0 +1,22 @@
+import * as API from '@/api/API';
+import AuthService from '@/services/AuthService';
+
+export default async function DeletePetAPI(
+{
+	id
+}: {
+	id: string
+}) {
+	API.GetCSRFToken();
+	const authToken = AuthService.getToken();
+	const url = API.GetAPIHost('/api/pet/delete');
+	const body = { 
+		id
+	};
+	console.log(body);
+	const res = await API.PostAPI(url, body, {
+		'Authorization': `Bearer ${authToken}`
+	});
+
+	return res
+}
