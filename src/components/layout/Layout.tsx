7d231fa440a51c89627d4eb15c5b0417cd9488b4
husinassegaff <husinassegaff15@gmail.com>
2022-05-28 13:21:08 +0700

    feat: update layout template & homepage

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
+
 import Header from '@/components/layout/Header';
+
 import { AuthProvider } from '@/providers/AuthContextProvider';
+
+import Footer from './Footer';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
-  return <>
-	<AuthProvider>
-		<Header/>
-		{children}
-	</AuthProvider>
-  </>;
+  return (
+    <>
+      <AuthProvider>
+        <Header />
+        {children}
+        <Footer />
+      </AuthProvider>
+    </>
+  );
 }
