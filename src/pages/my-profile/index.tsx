0955de20f0dbb19cc0d4752ed15e33577bfd1142
Syafiqjos <syafiqjos@gmail.com>
2022-05-28 14:06:24 +0700

    feat(myprofile): add base update myprofile page and form

---
+++ src/pages/my-profile/index.tsx
@@ -0,0 +1,175 @@
+import * as React from 'react';
+
+import AuthAPI from '@/api/AuthAPI';
+
+import Layout from '@/components/layout/Layout';
+import ArrowLink from '@/components/links/ArrowLink';
+import ButtonLink from '@/components/links/ButtonLink';
+import UnderlineLink from '@/components/links/UnderlineLink';
+import UnstyledLink from '@/components/links/UnstyledLink';
+import Seo from '@/components/Seo';
+
+import InputText from '@/components/forms/InputText';
+import InputButton from '@/components/forms/InputButton';
+
+import UnauthorizedRedirect from '@/components/auths/UnauthorizedRedirect';
+
+import { useAuthState, useAuthDispatch } from '@/providers/AuthContextProvider';
+import AuthService from '@/services/AuthService';
+
+import Link from 'next/link';
+
+/**
+ * SVGR Support
+ * Caveat: No React Props Type.
+ *
+ * You can override the next-env if the type is important to you
+ * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
+ */
+import Vercel from '~/svg/Vercel.svg';
+
+// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
+// Before you begin editing, follow all comments with `STARTERCONF`,
+// to customize the default configuration.
+
+function CustomButton({ onClick, children, active } : { onClick: any, children: any, active?: boolean }) {
+	const classNames = 'p-2 rounded-lg cursor-pointer';
+	const activeClassNames = 'bg-orange-600 text-white';
+
+	return (<li onClick={onClick} className={`${classNames} ${active ? activeClassNames : ''}`}>{children}</li>);
+}
+
+function UpdateProfileForm() {
+	const [email, setEmail] = React.useState('');
+	const [username, setUsername] = React.useState('');
+	const [name, setName] = React.useState('');
+	const [role, setRole] = React.useState('customer');
+	const [address, setAddress] = React.useState('');
+	const [phone, setPhone] = React.useState('');
+	const [gender, setGender] = React.useState('');
+	const [birthdate, setBirthdate] = React.useState<Date>(new Date());
+
+	React.useEffect(() => {
+		(async () => {
+			const token = AuthService.getToken();
+			const res = await AuthAPI({token});
+			const success = res.success;
+			if (success) {
+				const profile = res.data;
+				setEmail(profile.email);
+				setUsername(profile.username);
+				setName(profile.name);
+				setPhone(profile.phone);
+				setAddress(profile.address);
+				setGender(profile.gender);
+				setBirthdate(profile.birthdate);
+				setRole(profile.role);
+			}
+		})();
+	}, []);
+
+	function handleEmail(e: any){
+		setEmail(e.target.value);
+		setUsername(e.target.value);
+	}
+
+	function handlePassword(e: any){
+		setPassword(e.target.value);
+	}
+
+	function handlePasswordConf(e: any){
+		setPasswordConf(e.target.value);
+	}
+
+	function handleName(e: any) {
+		setName(e.target.value);
+	}
+
+	function handleAddress(e: any) {
+		setAddress(e.target.value);
+	}
+
+	function handlePhone(e: any) {
+		setPhone(e.target.value);
+	}
+
+	function handleGender(e: any) {
+		setGender(e.target.value);
+	}
+
+	function handleBirthdate(e: any) {
+		setBirthdate(e.target.value);
+	}
+
+	function handleSubmit(e: any) {
+		e.preventDefault();
+		console.log('submit update profile');
+	}
+
+	return <form className='flex flex-col items-start justify-start p-4 text-left gap-3' onSubmit={handleSubmit}>
+					  <h1 className="text-xl font-semibold">Perbarui profile</h1>
+					  <h2 className="text-base font-normal">Dapatkan sensasi hewan peliharaan.</h2>
+					  <InputText label="Email" name="email" type="text" placeholder="Email anda" disabled value={email} />
+					  <InputText label="Nama" name="name" type="text" placeholder="Nama anda" value={name} onChange={handleName} />
+					  <InputText label="Tanggal lahir" name="birthdate" type="date" placeholder="Tanggal lahir anda" value={birthdate} onChange={handleBirthdate} />
+					  <div className="flex flex-col items-start w-full">
+						<label htmlFor="gender">Jenis kelamin</label>
+						<select name="gender" id="gender" onChange={handleGender} className="border-0 rounded-l w-full p-4 bg-gray-100" value={gender}>
+							<option value="" disabled>Pilih jenis kelamin anda..</option>
+							<option value="m">Laki - laki</option>
+							<option value="f">Perempuan</option>
+						</select>
+					  </div>
+					  <InputText label="No. HP" name="phone" type="text" placeholder="No. HP anda" onChange={handlePhone} value={phone} />
+					  <InputText label="Alamat" name="address" type="text" placeholder="Alamat anda" onChange={handleAddress} value={address} />
+					  <InputButton text="Perbarui" />
+					</form>;
+}
+
+export default function HomePage() {
+  function handleSeeProfileButton(e: any) {
+	console.log('see profile button');
+  }
+
+  function handleUpdateProfileButton(e: any) {
+	console.log('update profile button');
+  }
+
+  function handleUpdatePasswordButton(e: any) {
+	console.log('update password button');
+  }
+
+  function handleLogoutButton(e: any) {
+	console.log('logout');
+  }
+
+  return (
+    <>
+      {/* <Seo templateTitle='Home' /> */}
+      <Seo />
+
+      <main>
+		
+			<section className='bg-white'>
+			  <div className='layout grid grid-cols-1 mt-8 w-100'>
+				<h1 className="text-xl font-semibold mb-2">Profil saya</h1>
+				<div className="px-4 grid grid-cols-4 gap-3">
+					<div className="flex flex-col gap-1">
+						<ul className="p-4">
+							<CustomButton onClick={handleSeeProfileButton}>Lihat Profile</CustomButton>
+							<CustomButton onClick={handleUpdateProfileButton} active>Perbarui Profile</CustomButton>
+							<CustomButton onClick={handleUpdatePasswordButton}>Ubah Password</CustomButton>
+							<CustomButton onClick={handleLogoutButton}>Logout</CustomButton>
+						</ul>
+					</div>
+					<div className="p-4 grid grid-cols-1 col-span-3">
+					  <UpdateProfileForm />
+					</div>
+				</div>
+			  </div>
+			</section>
+		
+      </main>
+    </>
+  );
+}
