aea8eda617f6bd0d54632ca4f9fb19ff7030a383
Syafiqjos <syafiqjos@gmail.com>
2022-05-28 09:28:31 +0700

    fix(app): implement header and layout in root app index instead

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app';
 
+import Layout from '@/components/layout/Layout';
+
 import '@/styles/globals.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
 import '@/styles/colors.css';
@@ -10,7 +12,7 @@ import '@/styles/colors.css';
  */
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Layout><Component {...pageProps} /></Layout>;
 }
 
 export default MyApp;
