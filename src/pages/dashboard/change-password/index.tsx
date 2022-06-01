3e32ca28e5981079da6a72e7017d21ec20eafc65
husinassegaff <husinassegaff15@gmail.com>
2022-06-01 14:02:23 +0700

    feat: refactor profile (change-password)

---
+++ src/pages/dashboard/change-password/index.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+
+import InputButton from '@/components/forms/InputButton';
+import InputText from '@/components/forms/InputText';
+import Sidebar from '@/components/layout/Sidebar';
+import Seo from '@/components/Seo';
+
+import ChangeUserPasswordAPI from '@/api/ChangeUserPasswordAPI';
+
+export default function ChangePassword({ onSubmit }: { onSubmit?: any }) {
+  const [oldPassword, setOldPassword] = React.useState('');
+	const [password, setPassword] = React.useState('');
+	const [passwordConf, setPasswordConf] = React.useState('');
+
+	function handleOldPassword(e: any) {
+		setOldPassword(e.target.value);
+	}
+
+	function handlePassword(e: any) {
+		setPassword(e.target.value);
+	}
+
+	function handlePasswordConf(e: any) {
+		setPasswordConf(e.target.value);
+	}
+
+	async function handleSubmit(e: any) {
+		e.preventDefault();
+		console.log('submit change password');
+
+		const res = await ChangeUserPasswordAPI({ old_password: oldPassword, password: password, password_confirmation: passwordConf });
+		console.log(res);
+		const success = res.success;
+		if (success) {
+			console.log('change password success');
+		} else {
+			console.log('change password failed');
+		}
+
+		if (onSubmit !== undefined) {
+			onSubmit();
+		}
+	}
+
+	return <>
+    <Seo title="Change Password"/>
+
+    <div className="flex">
+      <div className="flex-col">
+        <Sidebar />
+      </div>
+      <div className="flex-col pl-5 pt-5">
+        <form className='flex flex-col items-start justify-start p-4 text-left gap-3' onSubmit={handleSubmit}>
+          <h1 className="text-xl font-semibold">Ubah password</h1>
+          <h2 className="text-base font-normal">Dapatkan sensasi hewan peliharaan.</h2>
+          <InputText label="Password lama" name="old_password" type="password" placeholder="Password lama anda" value={oldPassword} onChange={handleOldPassword} />
+          <InputText label="Password baru" name="password" type="password" placeholder="Password baru anda" value={password} onChange={handlePassword} />
+          <InputText label="Ketik ulang password baru anda" name="password_confirmation" type="password" placeholder="Password baru anda" value={passwordConf} onChange={handlePasswordConf} />
+          <InputButton text="Ubah password" />
+        </form>
+      </div>
+    </div>
+  </>
+}
