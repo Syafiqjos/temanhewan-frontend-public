62453b7d37034458cc3f3be22f8449f75078f67f
husinassegaff <husinassegaff15@gmail.com>
2022-06-01 13:59:50 +0700

    feat: refactor profile (logout)

---
+++ src/pages/dashboard/logout/index.tsx
@@ -0,0 +1,43 @@
+import { useRouter } from 'next/router';
+import * as React from 'react';
+
+import InputButton from '@/components/forms/InputButton';
+import Sidebar from '@/components/layout/Sidebar';
+import Seo from '@/components/Seo';
+
+import LogoutAPI from '@/api/LogoutAPI';
+import { useAuthDispatch } from '@/providers/AuthContextProvider';
+
+
+export default function Logout() {
+	const router = useRouter();
+	const authDispatch = useAuthDispatch();
+
+	async function handleSubmit(e: any) {
+		e.preventDefault();
+		console.log('submit logout');
+		const res = await LogoutAPI();
+		console.log(res);
+		const success = res.success;
+		if (success) {
+			authDispatch({ type:'LOGOUT' });
+			router.push('/');
+		}
+	}
+	return <>
+    <Seo title="Logout"/>
+
+    <div className="flex">
+      <div className="flex-col">
+        <Sidebar />
+      </div>
+      <div className="flex-col pl-5 pt-5">
+      <form className='flex flex-col items-start justify-start p-4 text-left gap-3' onSubmit={handleSubmit}>
+        <h1 className="text-xl font-semibold">Logout Akun</h1>
+        <h2 className="text-base font-normal">Silahkan klik tombol dibawah untuk logout dari akun anda.</h2>
+        <InputButton text="Logout" />
+      </form>
+      </div>
+    </div>
+  </>
+}
