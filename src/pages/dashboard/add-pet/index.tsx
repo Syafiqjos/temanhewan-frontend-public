e991f4c808522914dbc7716a3fda6ecf6199f947
husinassegaff <husinassegaff15@gmail.com>
2022-06-06 00:12:19 +0700

    feat: add view add-pet on dashboard

---
+++ src/pages/dashboard/add-pet/index.tsx
@@ -0,0 +1,215 @@
+import { useRouter } from 'next/router'
+import * as React from 'react';
+
+import InputText from '@/components/forms/InputText';
+import Sidebar from '@/components/layout/Sidebar';
+import Seo from '@/components/Seo';
+
+import CreatePetAPI from '@/api/CreatePetAPI';
+import PetType from '@/enums/PetType';
+import Pet from '@/interfaces/Pet';
+
+/**
+ * SVGR Support
+ * Caveat: No React Props Type.
+ *
+ * You can override the next-env if the type is important to you
+ * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
+ */
+
+// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
+// Before you begin editing, follow all comments with `STARTERCONF`,
+// to customize the default configuration.
+
+function getPetType(petType: PetType | string) {
+	switch (petType) {
+		case PetType.Cat:
+			return 'Kucing';
+		case PetType.Dog:
+			return 'Anjing';
+	}
+	return 'Awokawok';
+}
+
+function getPetRace(petType: PetType | string) {
+	switch (petType) {
+		case PetType.Cat:
+		case 'Cat':
+			return 'cat';
+		case PetType.Dog:
+		case 'Dog':
+			return 'dog';
+	}
+	return 'Awokawok';
+}
+
+function getGender(gender: string) {
+	switch (gender) {
+		case 'm':
+			return 'Jantan';
+		case 'f':
+			return 'Betina';
+	}
+	return 'Awokawok';
+}
+
+function InitialPage({ router, setMyPet, setErrorMessage, setStatus }: { router: any, setMyPet: any, setErrorMessage: any, setStatus: any }) {
+
+	const profileImageInput = React.useRef(null);
+	const [name, setName] = React.useState('');
+	const [description, setDescription] = React.useState('');
+	const [gender, setGender] = React.useState('m');
+	const [petType, setPetType] = React.useState(0);
+
+	function getProfileImage(){
+		const input: any = profileImageInput.current!;
+		if (input.files && input.files.length > 0) {
+			return input.files[0];
+		}
+
+		return undefined;
+	}
+
+	async function handleSubmit(e: any) {
+		e.preventDefault();
+		let petId = name.toLowerCase().replace(' ', '-');
+		
+		const res = await CreatePetAPI({
+			name,
+			description,
+			gender,
+			race: getPetRace(petType),
+			profile_image: getProfileImage()
+		});
+
+		// Submit pet and get pet id from server
+		const success = res.success;
+		if (success) {
+			petId = res.data.id;
+			setMyPet({
+				id: petId,
+				name: name,
+				description: description,
+				race: petType,
+				gender: gender,
+				imageUrl: res.data.profile_image
+			});
+			setStatus('SUCCESS');
+		} else {
+			setErrorMessage('Something went wrong!');
+			setStatus('ERROR');
+		}
+		setTimeout(() => {router.push('/dashboard/my-pet/i/' + petId)} , 1000);
+	}
+
+	function handleSetName(e: any) {
+		setName(e.target.value);
+	}
+
+	function handleSetDescription(e: any) {
+		setDescription(e.target.value);
+	}
+
+	function handleSetPetType(e: any) {
+		setPetType(e.target.value);
+	}
+
+	function handleSetGender(e: any) {
+		setGender(e.target.value);
+	}
+
+	return (<>
+      <form className="flex flex-col gap-1">
+        <ul className="p-4">
+          <img className="rounded-xl object-cover w-full h-48" src="https://api-temanhewan.mirzaq.com/image/pet_default.png" />
+        </ul>
+      </form>
+      <form className="p-4 grid grid-cols-1 gap-2" onSubmit={handleSubmit}>
+        <div className="flex flex-col items-start w-full">
+          <label htmlFor="petType">Foto Peliharaan</label>
+          <input ref={profileImageInput} name="profile_image" type="file" accept="image/*" />
+        </div>
+        <InputText label="Nama" name="name" type="text" value="" onChange={handleSetName} />
+        <div className="flex flex-col items-start w-full">
+          <label htmlFor="petType">Jenis Peliharaan</label>
+          <select className="w-full" id="petType" onChange={handleSetPetType}>
+          {Object.keys(PetType).filter((v) => isNaN(Number(v))).map((v, i) => 
+            (<option key={`petType-${v}`} value={v}>{getPetType(i)}</option>)
+          )}
+          </select>
+        </div>
+
+        <div className="flex flex-col items-start w-full">
+          <label htmlFor="gender">Jenis Kelamin</label>
+          <select className="w-full" id="gender" onChange={handleSetGender}>
+          {['m', 'f'].map((v, i) => 
+            (<option key={`gender-${v}`} value={v}>{getGender(v)}</option>)
+          )}
+          </select>
+        </div>
+
+        <div className="flex flex-col items-start w-full">
+          <label htmlFor="description">Deskripsi</label>
+          <textarea className="w-full" name="description"onChange={handleSetDescription} value={description} />
+        </div>
+
+        <input className="bg-orange-600 text-white font-semibold rounded-xl p-3" type="submit" value="Tambah +" />
+      </form>
+	</>);
+}
+
+function ErrorPage({ errorMessage }: { errorMessage: string }) {
+	return (<>
+	<div className="flex flex-col gap-1">
+		<ul className="p-4">
+			<img className="rounded-xl object-cover w-full h-48" src="/images/cover/register-cover.png" />
+		</ul>
+	</div>
+	<div className="p-4 grid grid-cols-1">
+	  <h1>{errorMessage}</h1>
+	</div>
+	</>);
+}
+
+function SuccessPage({ myPet }: { myPet: Pet }) {
+	return (<>
+	<div className="flex flex-col gap-1">
+		<ul className="p-4">
+			<img className="rounded-xl object-cover w-full h-48" src={myPet.imageUrl} />
+		</ul>
+	</div>
+	<div className="p-4 grid grid-cols-1">
+	  <h1>Peliharaan {myPet.name} ditambah!</h1>
+	</div>
+	</>);
+}
+
+export default function HomePage() {
+  const router = useRouter();
+  const [ status, setStatus ] = React.useState<'INITIAL' | 'ERROR' | 'SUCCESS'>('INITIAL');
+  const [ myPet, setMyPet ] = React.useState<Pet>({ id: '', name: '', race: PetType.Cat, gender: 'm', description: '' });
+  const [ errorMessage, setErrorMessage ] = React.useState<string>('');
+
+  return (
+    <>
+      {/* <Seo templateTitle='Home' /> */}
+      <Seo title = "Add Pet"/>
+
+      <Sidebar>
+      <main>
+        <section className='bg-white'>
+          <div className='layout grid grid-cols-1 mt-8 w-100'>
+			<h1 className="text-xl font-semibold mb-2">Tambah peliharaan saya</h1>
+            <div className="px-4 grid grid-cols-2 gap-3">
+				{errorMessage !== '' && <ErrorPage errorMessage={errorMessage} />
+				|| status === 'INITIAL' && <InitialPage router={router} setMyPet={setMyPet} setErrorMessage={setErrorMessage} setStatus={setStatus} />
+				|| status === 'SUCCESS' && <SuccessPage myPet={myPet} />
+				}
+            </div>
+		  </div>
+        </section>
+      </main>
+      </Sidebar>
+    </>
+  );
+}
