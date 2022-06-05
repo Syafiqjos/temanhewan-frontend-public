e2e9c00e32bcb11134138f35e19edb090980a293
Syafiqjos <syafiqjos@gmail.com>
2022-06-05 10:58:37 +0700

    feat(consultation): add customer phone field

--- src/components/business/consultations/CustomerFormComponent.tsx
+++ src/components/business/consultations/CustomerFormComponent.tsx
@@ -6,6 +6,7 @@ function CustomerFormComponent({ user }: { user: any }) {
 	return <form className="border rounded-lg border-orange-600 p-8 mb-4">
 			<h1 className="text-lg mb-4">Pelanggan</h1>
 			<InputText label="Email Pelanggan" name="customer_email" value={user.email} disabled></InputText>
+			<InputText label="No. Telp Pelanggan" name="customer_phone" value={user.phone} disabled></InputText>
 			<InputText label="Nama Pelanggan" name="customer_name" value={user.name} disabled></InputText>
 		</form>;
 }
