a36bed4b3734fa7469298e307d1df9329c1f9281
husinassegaff <husinassegaff15@gmail.com>
2022-06-08 00:20:25 +0700

    feat: update forum view

--- src/pages/forum/index.tsx
+++ src/pages/forum/index.tsx
@@ -22,8 +22,8 @@ export default function ForumView() {
       <Seo title='Forum' />
 
       <main>
-        <section className='bg-white py-10 2xl:py-40'>
-          <div className='container mx-auto px-4'>
+        <section className='bg-white py-10 2xl:py-40 layout'>
+          <div className='container mx-auto'>
             <div className='mb-5 text-center'>
               <h2 className='font-heading mt-8 text-7xl font-bold'>Forum</h2>
             </div>
@@ -31,11 +31,15 @@ export default function ForumView() {
               return (
                 <div key={forum.id}>
                   <CardForum
+                    author = {forum.author.name}
+                    avatar = {forum.author.avatar}
+                    date = {forum.updated_at.split(' ')[0]}
                     image = {forum.forum_images ? forum.forum_images[0] : '/images/image_post.png'}
-                    slug={forum.slug}
+                    slug={'/forum/i/' + forum.id}
                     title={forum.title}
                     subtitle={forum.subtitle}
                     content={forum.content.length > 200 ? forum.content.substring(0, 200) + '...' : forum.content}
+                    
                   />
                 </div>
               );
