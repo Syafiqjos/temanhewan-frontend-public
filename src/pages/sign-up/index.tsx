a2522993ecd4190d11b3d4cd48ce3d89c1ee9eae
husinassegaff <husinassegaff15@gmail.com>
2022-06-08 00:21:24 +0700

    feat: update register view

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
 
-import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
-import InputText from '@/components/forms/InputText';
+import UnauthorizedRedirect from '@/components/auths/UnauthorizedRedirect';
 import InputButton from '@/components/forms/InputButton';
+import InputText from '@/components/forms/InputText';
+import UnstyledLink from '@/components/links/UnstyledLink';
 import Seo from '@/components/Seo';
 
-import UnauthorizedRedirect from '@/components/auths/UnauthorizedRedirect';
-
-import RegisterAPI from '@/api/RegisterAPI';
 import LoginAPI from '@/api/LoginAPI';
-import { useAuthState, useAuthDispatch } from '@/providers/AuthContextProvider';
+import RegisterAPI from '@/api/RegisterAPI';
+import { useAuthDispatch,useAuthState } from '@/providers/AuthContextProvider';
 import AuthService from '@/services/AuthService';
 
 /**
@@ -23,7 +18,6 @@ import AuthService from '@/services/AuthService';
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Vercel from '~/svg/Vercel.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
@@ -142,20 +136,19 @@ function RegisterForm() {
 export default function HomePage() {
   return (
     <>
-      {/* <Seo templateTitle='Home' /> */}
-      <Seo />
+      <Seo title = "Register" />
 
       <main>
-		<UnauthorizedRedirect>
-			<section className='bg-white'>
-			  <div className='layout min-h-screen grid grid-cols-2 mt-8 w-100'>
-				<RegisterForm/>
-				<div className="p-4">
-				  <img className="rounded-xl" src="/images/cover/register-cover.png" />
-				</div>
-			  </div>
-			</section>
-		</UnauthorizedRedirect>
+				<UnauthorizedRedirect>
+					<section className='bg-white'>
+						<div className='layout min-h-screen grid grid-cols-2 mt-8 w-100'>
+						<RegisterForm/>
+						<div className="p-4">
+							<img className="rounded-xl" src="/images/cover/register-cover.png" alt="register image" />
+						</div>
+						</div>
+					</section>
+				</UnauthorizedRedirect>
       </main>
     </>
   );
