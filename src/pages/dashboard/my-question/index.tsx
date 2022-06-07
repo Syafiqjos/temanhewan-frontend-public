4918ae1ac57f9b8efbbe3d8d34cd7556a4e7161a
husinassegaff <husinassegaff15@gmail.com>
2022-06-08 00:20:11 +0700

    feat: update dashboard my-question view

--- src/pages/dashboard/my-question/index.tsx
+++ src/pages/dashboard/my-question/index.tsx
@@ -32,7 +32,7 @@ export default function MyForum() {
                   <div key={forum.id}>
                     <CardForum
                       image = {forum.forum_images ? forum.forum_images[0] : '/images/image_post.png'}
-                      slug={forum.slug}
+                      slug={'/dashboard/my-question/i/' + forum.id}
                       title={forum.title}
                       subtitle={forum.subtitle}
                       content={forum.content.length > 200 ? forum.content.substring(0, 200) + '...' : forum.content}
