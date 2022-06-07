bfb6b69e60c5dacf8dd3764ad5878bbb64460f9a
husinassegaff <husinassegaff15@gmail.com>
2022-06-08 00:20:59 +0700

    feat: update login view

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
 
-import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
-import InputText from '@/components/forms/InputText';
+import UnauthorizedRedirect from '@/components/auths/UnauthorizedRedirect';
 import InputButton from '@/components/forms/InputButton';
+import InputText from '@/components/forms/InputText';
+import UnstyledLink from '@/components/links/UnstyledLink';
 import Seo from '@/components/Seo';
 
-import UnauthorizedRedirect from '@/components/auths/UnauthorizedRedirect';
-
 import LoginAPI from '@/api/LoginAPI';
-import { useAuthState, useAuthDispatch } from '@/providers/AuthContextProvider';
+import { useAuthDispatch,useAuthState } from '@/providers/AuthContextProvider';
 import AuthService from '@/services/AuthService';
 
 /**
@@ -22,7 +17,6 @@ import AuthService from '@/services/AuthService';
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Vercel from '~/svg/Vercel.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
@@ -74,19 +68,18 @@ function LoginForm() {
 export default function HomePage() {
   return (
     <>
-      {/* <Seo templateTitle='Home' /> */}
-      <Seo />
+      <Seo title="Login" />
       <main>
         <UnauthorizedRedirect>
-			<section className='bg-white'>
-			  <div className='layout min-h-screen grid grid-cols-2 mt-8 w-100'>
-				<LoginForm/>
-				<div className="p-4">
-				  <img className="rounded-xl" src="/images/cover/login-cover.png" />
-				</div>
-			  </div>
-			</section>
-	    </UnauthorizedRedirect>
+					<section className='bg-white'>
+						<div className='layout min-h-screen grid grid-cols-2 mt-8 w-100'>
+						<LoginForm/>
+						<div className="p-4">
+							<img className="rounded-xl" src="/images/cover/login-cover.png" alt="login image" />
+						</div>
+						</div>
+					</section>
+				</UnauthorizedRedirect>
       </main>
     </>
   );
