aaffa59a0dd2172d675cd81c60119dde7b76ef9b
husinassegaff <husinassegaff15@gmail.com>
2022-06-08 00:19:29 +0700

    feat: update dashboard logout view

--- src/pages/dashboard/logout/index.tsx
+++ src/pages/dashboard/logout/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import * as React from 'react';
 
+import ShouldAuthorized from '@/components/auths/ShouldAuthorized';
 import InputButton from '@/components/forms/InputButton';
 import Sidebar from '@/components/layout/Sidebar';
 import Seo from '@/components/Seo';
@@ -28,11 +29,15 @@ export default function Logout() {
     <Seo title="Logout"/>
 
     <Sidebar>
-      <form className='flex flex-col items-start justify-start p-4 text-left gap-3' onSubmit={handleSubmit}>
-        <h1 className="text-xl font-semibold">Logout Akun</h1>
-        <h2 className="text-base font-normal">Silahkan klik tombol dibawah untuk logout dari akun anda.</h2>
-        <InputButton text="Logout" />
-      </form>
+			<main>
+				<ShouldAuthorized>
+				<form className='flex flex-col items-start justify-start p-4 text-left gap-3' onSubmit={handleSubmit}>
+					<h1 className="text-xl font-semibold">Logout Akun</h1>
+					<h2 className="text-base font-normal">Silahkan klik tombol dibawah untuk logout dari akun anda.</h2>
+					<InputButton text="Logout" />
+				</form>
+				</ShouldAuthorized>
+			</main>
     </Sidebar>
   </>
 }
