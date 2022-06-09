69bc51abf2403a8f5aff2eb9def0a3d3802ccc5c
Syafiqjos <syafiqjos@gmail.com>
2022-06-09 07:32:22 +0700

    feat(component): add grooming order review form component

---
+++ src/components/business/groomings/ReviewFormComponent.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import InputText from '@/components/forms/InputText';
+import ButtonLink from '@/components/links/ButtonLink';
+
+function ReviewFormComponent({ review }: { review: any }) {
+	return <form className="border rounded-lg border-orange-600 p-8 mb-4">
+			<h1 className="text-lg mb-4">Review Pelanggan</h1>
+			<InputText label="Rating" name="review_rating" value={review.rating + " Star(s)"} disabled></InputText>
+			<textarea className="bg-white text-orange-600 rounded-xl border-orange-600 p-4 inline border-2 w-full mb-2" placeholder="Review pelanggan" defaultValue={review.review} disabled></textarea>
+			<InputText label="Publikasi" name="review_rating" value={review.is_public == 1 ? 'Review ditampilkan ke public' : 'Review bersifat privasi dan hanya ditunjukkan kepada jasa grooming'} disabled></InputText>
+		</form>;
+}
+
+export default ReviewFormComponent;
