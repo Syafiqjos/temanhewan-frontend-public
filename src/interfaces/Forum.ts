012e9f34d63fe48d7d12107412ce576773b4558f
husinassegaff <husinassegaff15@gmail.com>
2022-06-09 10:10:53 +0700

    feat: update interface Forum

--- src/interfaces/Forum.ts
+++ src/interfaces/Forum.ts
@@ -1,10 +1,10 @@
 export default interface Forum {
   id?: string;
-  slug: string;
+  slug?: string;
   title: string;
   subtitle: string;
   content: string;
-  forum_images?: string[];
+  forum_images: string[];
   author: Record<string, unknown>;
   updated_at: string;
 }
