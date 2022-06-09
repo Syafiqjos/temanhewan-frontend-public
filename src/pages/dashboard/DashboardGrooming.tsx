272817798f83f096ab3afad59f502bdfbc68fd5a
husinassegaff <husinassegaff15@gmail.com>
2022-06-09 10:45:22 +0700

    feat: add dashboard grooming

---
+++ src/pages/dashboard/DashboardGrooming.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import SidebarGroomer from '@/components/layout/SidebarGroomer';
+
+export default function DashboardGrooming() {
+  return (
+    <>      
+      <SidebarGroomer>
+        <main>  
+          <h1 className="text-xl font-semibold">Dashboard Grooming</h1>
+        </main>
+      </SidebarGroomer>
+     
+    </>
+  );
+}
